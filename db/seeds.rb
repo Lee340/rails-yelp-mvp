62a294a93ba096fe2dbe8744de90232ad4192632
Boram Lee <boramlee340@gmail.com>
2022-05-25 21:39:29 +0100

    second challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning databases"
+Restaurant.destroy_all
+
+10.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_name,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  puts "Restaurant with id:#{restaurant.id} has been created"
+end
